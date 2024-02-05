ae17e9d0bc405d70516491df0525e2b2b500b27b
NotLeo27 <leoo12@nycstudents.net>
2024-02-05 13:59:15 -0500

    Update tipcalculator.py

--- tipcalculator.py
+++ tipcalculator.py
@@ -1,3 +1,12 @@
-subtotal=input()
+print('what is your subtotal?')
+subtotal=int(input())
+print('What is your tip percent?')
+tip_percent = int(input())
 
-print(subtotal)
+def Tip_calculator():
+    print(subtotal)
+    tip=tip_percent*subtotal/100
+    Total=tip+subtotal
+    print(Total)
+
+Tip_calculator()
