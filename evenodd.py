0c16161a01ac2dcf2c8c5f977b2179892d90951e
NotLeo27 <leoo12@nycstudents.net>
2024-02-05 13:59:04 -0500

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+#Creates a function 
+
+print("What number would you look to refer to?")
+number=float(input())
+
+if(number % 2 == 0):
+    print('Your number is even!')
+
+else:
+    print("It is an odd number")
