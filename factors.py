bc938c0662717845f643ff0788decc140f343b58
NotLeo27 <leoo12@nycstudents.net>
2024-02-07 14:00:41 -0500

    Create factors.py

---
+++ factors.py
@@ -0,0 +1,9 @@
+#reate a funtion that accepts an input and determines all factors of the number
+num=int(input('Enter the number you want inputted: '))
+def factorlization():
+    print(f'The factors of {num} are: ')
+    for i in range(1, num + 1):
+        if num % i == 0:
+            print(i)
+        
+factorlization()
