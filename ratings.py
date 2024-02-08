dbfa31c22f9b7c57569398193251760579b8f435
NotLeo27 <leoo12@nycstudents.net>
2024-02-08 13:49:00 -0500

    Update ratings.py

--- ratings.py
+++ ratings.py
@@ -1,12 +1,18 @@
 #Let's create a function to accept a "bill" value and offer a tip of 0%, 15%, 20% or 25% depending on if the service was "bad, okay, good , or great ".
 
-def subtotal1():
-    global subtotal1
-    subtotal = float(input("> $"))
+
+subtotal = float(input("> $"))
+print('How was our services?')
 
 def ratings():
-    input("How was our services")
-    if ratings == "Bad":
+    services = input()
+    if services == "Bad":
         print("I'm sorry to hear that, you won't need to tip at all!")
-    elif ratings == "Okay"
+    elif services == "Okay":
+        print("Thank you! We recommend a tip of", subtotal*0.15)
+    elif services == "Good":
+        print("Thank you! We recommend a tip of", subtotal*0.20)
+    elif services == "Great":
+        print("Thank you! We recommend a tip of", subtotal*0.25)
+
 ratings()
